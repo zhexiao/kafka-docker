0dcec33111dff6dad9dab001c42f7a5a3fbf3f2f
zhexiao <zhexiao@163.com>
2019-07-24 18:22:30 +0800

    add config

--- kafka_configuration.sh
+++ kafka_configuration.sh
@@ -1,12 +1,18 @@
-#!/bin/bash -e
+#!/usr/bin/env bash
 
 function update_config() {
-  # 更新配置文件的函数
-  env_name=$1
-  env_val=$2
-  config_file=$3
+    # 更新配置文件的函数
+    env_name=$1
+    env_val=$2
+    config_file=$3
 
-  echo "$env_name $env_val $config_file"
+    echo "[Configuring] $env_name in $config_file"
+    
+    # 如果配置名在文件里面找到了，则直接替换
+    if grep -E  "^#?$env_name=.*" "$config_file"
+    then
+    	echo "find env in file"
+    fi
 }
 
 # 不允许出现的配置参数
@@ -21,15 +27,14 @@ env_name=$(echo "$env_var" | cut -d "=" -f 1)
 # 使用双括号可以用正则匹配符
 if [[ $NOT_ALLOWED_ENV = *"|$env_name|"* ]]
 then
-  echo "not allowed env $env_name"
-  continue
+    echo "[Invalid Env] $env_name"
+    continue
 fi
 
 if [[ $env_name =~ ^KAFKA_ ]]
 then
-  echo "kafka config $env_name"
-  env_val=$(echo "$env_var" | cut -d "=" -f 2)
-  update_config "$env_name" "$env_val" "config/abc.properties"
+    env_val=$(echo "$env_var" | cut -d "=" -f 2)
+    update_config "$env_name" "$env_val" "server.properties.3"
 fi
 
 done
